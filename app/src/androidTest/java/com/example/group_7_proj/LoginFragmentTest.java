aa80832d95639bf30e7e148604d86bd117038668
mukaddim98 <mukaddim1998@gmail.com>
2020-10-04 05:26:45 -0300

    Initial UI testing for AT-12.1 done

--- app/src/androidTest/java/com/example/group_7_proj/LoginFragmentTest.java
+++ app/src/androidTest/java/com/example/group_7_proj/LoginFragmentTest.java
@@ -9,7 +9,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
