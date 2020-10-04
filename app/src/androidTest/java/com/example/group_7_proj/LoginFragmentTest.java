fdd518b9316f9bc3532c7304efc0e011763c1406
mukaddim98 <mukaddim1998@gmail.com>
2020-10-04 04:54:20 -0300

    Added navigation to dashboard and empty email UI tests

--- app/src/androidTest/java/com/example/group_7_proj/LoginFragmentTest.java
+++ app/src/androidTest/java/com/example/group_7_proj/LoginFragmentTest.java
@@ -1,4 +1,43 @@
 package com.example.group_7_proj;
 
-public class LoginFragmentTest {
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class LoginFragmentTest{
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    // checks if the email text box is empty and displays appropriate message
+    public void emptyEmail() {
+        onView(withId(R.id.emailText)).perform(click()).perform(typeText(""));
+        onView(withId(R.id.passwordText)).perform(click());
+        onView(withId(R.id.emailHint)).check(matches(withText("Email address missing")));
+    }
+
+    @Test
+    // checks if app is able to successfully navigate to dashboard
+    public void navToDashboard() {
+        onView(withId(R.id.emailText)).perform(click()).perform(typeText("abc@xyz.com"));
+        onView(withId(R.id.passwordText)).perform(click()).perform(typeText("abcDEF123!@#"));
+        onView(withId(R.id.loginBtn)).perform(click());
+        onView(withID(R.id.dashboard));
+    }
 }
--- app/src/main/java/com/example/group_7_proj/MainActivity.java
+++ app/src/main/java/com/example/group_7_proj/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -19,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
