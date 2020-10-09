a37bb49497a84a2fe040396cca3932d27b372a95
mukaddim98 <mukaddim1998@gmail.com>
2020-10-09 16:35:56 -0300

    Changed main activity to previous version

--- app/src/main/java/com/example/group_7_proj/MainActivity.java
+++ app/src/main/java/com/example/group_7_proj/MainActivity.java
@@ -1,54 +1,56 @@
 package com.example.group_7_proj;
 
-import android.content.DialogInterface;
 import android.os.Bundle;
-import android.view.View;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.fragment.NavHostFragment;
+import androidx.appcompat.widget.Toolbar;
 
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_main);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        ImageButton googleLoginBtn =(ImageButton)findViewById(R.id.googleLoginBtn);
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-
-        /*googleLoginBtn.setOnClickListener(new View.OnClickListener(){
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
 
-                    builder.setMessage("The sum is "+resultTextView.getText().toString()).setTitle("Addition Operation")
-                            .setNeutralButton("OK", new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int id){
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
-                                }
-                            });
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
-                    //Creating dialog box
-                    AlertDialog alert = builder.create();
-                    alert.show();
-                }
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
 
-        });*/
-
-
+        return super.onOptionsItemSelected(item);
     }
 }
