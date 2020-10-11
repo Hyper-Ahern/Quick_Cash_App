fa5f8fb30f871ab8fcc202e69bfb90878e8dab5b
Renayre <>
2020-10-11 16:20:54 -0300

    moved signup fragment code to sigupactivity file

--- app/src/main/java/com/example/group_7_proj/SignUpActivity.java
+++ app/src/main/java/com/example/group_7_proj/SignUpActivity.java
@@ -1,35 +1,21 @@
 package com.example.group_7_proj;
 
-import android.content.DialogInterface;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
+import com.example.group_7_proj.CustomDataTypes.Email;
+import com.example.group_7_proj.CustomDataTypes.Password;
+import com.example.group_7_proj.CustomDataTypes.Username;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+;
 
-import android.view.KeyEvent;
-import android.view.View;
-
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
+
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.example.group_7_proj.CustomDataTypes.*;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
-import java.net.PasswordAuthentication;
+
 
 public class SignUpActivity extends AppCompatActivity {
     @Override
@@ -37,4 +23,46 @@ public class SignUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup_main);
     }
+
+    private Button submitBtn;
+    private Button captchaBtn;
+
+    private EditText etUName  = findViewById(R.id.name);
+    private EditText etEmail  = findViewById(R.id.email);
+    private EditText etPassword = findViewById(R.id.password);
+
+
+    private Username name = new Username((etUName).getText().toString());
+    private Email email = new Email((etEmail).getText().toString());
+    private Password password = new Password((etPassword).getText().toString());
+
+    private Boolean nameValid = false;
+    private Boolean emailValid = false;
+    private Boolean passwordValid = false; //password.isWeak();
+
+    private TextView nameErrorMessage = findViewById(R.id.nameErrorMessage);
+    private TextView emailErrorMessage = findViewById(R.id.emailErrorMessage);
+    private TextView passwordErrorMessage = findViewById(R.id.passwordErrorMessage);
+    private TextView reenterPasswordErrorMessage = findViewById(R.id.passwordErrorMessage2);
+
+   // protected void displayErrorMsg(TextView errorMessage){
+        //errorMessage.setVisibility(View.visible);
+    //}
+
+    //@Override
+    protected void onValid(){
+        if (nameValid && emailValid && passwordValid) {
+            submitBtn.setEnabled(true);
+        }
+    }
+
+    //protected void onSubmit(){
+       // submitBtn.setOnClickListener(new View.OnClickListner(){
+            //@Override
+            //public void onClick(View view){
+                //CHECK If fields valid, if so submit, if not do not submit
+
+            //}
+        //});
+    //}
 }
