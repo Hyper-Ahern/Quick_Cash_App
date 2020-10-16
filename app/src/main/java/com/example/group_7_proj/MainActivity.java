9ec5a9996f1232abef8699e8454ad3aca1046799
mukaddim98 <mukaddim1998@gmail.com>
2020-10-15 21:21:23 -0300

    Pull from master

--- app/src/main/java/com/example/group_7_proj/MainActivity.java
+++ app/src/main/java/com/example/group_7_proj/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+        Intent intent = new Intent(getApplicationContext(), PaymentInfoUI.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/group_7_proj/PaymentInfoUI.java
+++ app/src/main/java/com/example/group_7_proj/PaymentInfoUI.java
@@ -1,10 +1,9 @@
 package com.example.group_7_proj;
 
-import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 
-import com.google.firebase.database.DataSnapshot;
+import com.example.group_7_proj.CustomDataTypes.Card;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -13,8 +12,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
