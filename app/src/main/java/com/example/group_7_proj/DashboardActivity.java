2e5b8a50330e1eee7197f5868829684ef8c9d062
Hyper A'Hern <wl295462@dal.ca>
2020-10-27 19:53:48 -0300

    Add request location permission functionality

--- app/src/main/java/com/example/group_7_proj/DashboardActivity.java
+++ app/src/main/java/com/example/group_7_proj/DashboardActivity.java
@@ -1,13 +1,30 @@
 package com.example.group_7_proj;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class DashboardActivity extends AppCompatActivity {
     Button backBtn, postAJobBtn, payEmployeeBtn, allJobPostBtn;
@@ -15,7 +32,14 @@ public class DashboardActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        locationFinder();
         setContentView(R.layout.dashboard);
+
+        Button btLocation;
+        TextView textView;
+        Geocoder geocoder;
+        List<Address> addresses;
+        FusedLocationProviderClient fusedLocationProviderClient;
 
         backBtn = (Button)findViewById(R.id.backBtnDB);
         postAJobBtn = (Button)findViewById(R.id.postJobBtnDB);
@@ -53,6 +77,41 @@ public class DashboardActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
     }
+
+
+
+    public void locationFinder(){
+        if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED){
+            //then yes we run program
+        }
+        else {
+            Toast.makeText(this, "Location permission is needed to show you relevant " +
+                    "jobs near you.", Toast.LENGTH_SHORT).show();
+            requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+        }
+    }
+
+    public void onRequestPermissionsResult( int requestCode, String permissions [], int[] grantResults){
+        if (requestCode == 1) {//ACCESS_COARSE_LOCATION
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission granted!", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(this, "Location Permission not granted, the app will not " +
+                        "function normally without your location.", Toast.LENGTH_SHORT).show();
+            }
+        }
+        else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+
+
+
+
+
+
 }
