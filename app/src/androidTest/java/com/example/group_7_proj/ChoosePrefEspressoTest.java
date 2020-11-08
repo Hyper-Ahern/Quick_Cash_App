4d789d5655ba37f0f5d8fcb261b8fd3d8d22c187
Renayre <>
2020-11-08 15:25:13 -0400

    AT7.6 Espresso tests for choosing preferences

--- app/src/androidTest/java/com/example/group_7_proj/ChoosePrefEspressoTest.java
+++ app/src/androidTest/java/com/example/group_7_proj/ChoosePrefEspressoTest.java
@@ -2,21 +2,41 @@ package com.example.group_7_proj;
 
 import org.junit.Test;
 
+import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
 
 public class ChoosePrefEspressoTest {
 
+    //does selected/pressed match what's in selected list
+
     @Test
-    public void chooseSingleOption(){
-        onView(withId(R.id.multi_spinner)).perform(click()).perform();
+    public void chooseSingleOption(){//selects first option
+        onView(withId(R.id.multi_spinner)).perform(click());
+        onData(allOf(is(instanceOf(String.class)))).atPosition(0).perform(click());
+        onView(withId(R.id.etPickedJobTypes)).check(matches(withText("choosen type")));
+        onView(withId(R.id.btnPickJobPref)).perform(click());
+       // onData(allOf(is(instanceOf(String.class)), is(selectionText))).perform(click());
     }
 
     @Test
-    public void chooseNoOption(){}
+    public void chooseNoOption(){
+        onView(withId(R.id.multi_spinner)).perform(click());
+        onView(withId(R.id.btnPickJobPref)).perform(click());
+    }
 
     @Test
-    public void chooseMultipleOptions(){}
+    public void chooseMultipleOptions(){
+        onView(withId(R.id.multi_spinner)).perform(click());
+        onData(allOf(is(instanceOf(String.class)))).atPosition(0).perform(click());
+        onData(allOf(is(instanceOf(String.class)))).atPosition(1).perform(click());
+        onView(withId(R.id.etPickedJobTypes)).check(matches(withText("choosen types")));
+        onView(withId(R.id.btnPickJobPref)).perform(click());
+    }
 }
