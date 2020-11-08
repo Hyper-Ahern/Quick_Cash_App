4671fe274d26d710930add9377c75d0b41b92071
Renayre <>
2020-11-08 18:12:12 -0400

    create code to test espresso tests (start activity for choose pref) (AT7.6)

--- app/src/main/java/com/example/group_7_proj/ChooseJobPrefActivity.java
+++ app/src/main/java/com/example/group_7_proj/ChooseJobPrefActivity.java
@@ -3,6 +3,8 @@ package com.example.group_7_proj;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +12,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
+
 public class ChooseJobPrefActivity extends AppCompatActivity {
+    private Button submitBtn;
+    private CheckBox deliveryCategory, cleanCategory, compCategory, babysitCategory, otherCategory;
+
+    private ArrayList<String> selectedJobCategories;
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        submitBtn = findViewById(R.id.btnPickJobPref);
+
+        deliveryCategory = findViewById(R.id.ckbxDelivery);
+        cleanCategory = findViewById(R.id.ckbxClean);
+        compCategory = findViewById(R.id.ckbxComp);
+        babysitCategory = findViewById(R.id.ckbxBabysit);
+        otherCategory = findViewById(R.id.ckbxOther);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.choose_jobpref);
@@ -27,6 +42,9 @@ public class ChooseJobPrefActivity extends AppCompatActivity {
 
         final FirebaseDatabase database = FirebaseDatabase.getInstance();
         final DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference().child(firebaseFirstLevel).child(firebaseSecondLevel);
+
+
+        //submit btn onclick
 
         //Intent intent = new Intent(getApplicationContext(), DashboardActivity.class);
         //startActivity(intent);
--- app/src/main/java/com/example/group_7_proj/CustomDataTypes/User.java
+++ app/src/main/java/com/example/group_7_proj/CustomDataTypes/User.java
@@ -10,6 +10,7 @@ public class User {
     private Password password;
     private ArrayList<JobPost> appliedJobs;
     private ArrayList<JobPost> createdJobPosts;
+    private ArrayList<String> jobCategoryPreferences;
 
     public User() {
     }
@@ -20,6 +21,7 @@ public class User {
         this.password = password;
         this.appliedJobs = new ArrayList<JobPost>();
         this.createdJobPosts = new ArrayList<JobPost>();
+        this.jobCategoryPreferences = new ArrayList<String>();
     }
 
     public String getName() {
@@ -53,4 +55,10 @@ public class User {
     public void setCreatedJobPosts(ArrayList<JobPost> createdJobPosts){this.createdJobPosts = createdJobPosts;}
     public void addCreatedJobPosts(JobPost createdJobPost){this.createdJobPosts.add(createdJobPost);}
     public ArrayList<JobPost> getCreatedJobPosts(){return this.createdJobPosts;}
+    public void deleteCreatedJobPost(JobPost jobPost){this.createdJobPosts.remove(jobPost);}
+
+    public void setJobCategoryPreferences(ArrayList<String> jobCategoryPreferences){this.jobCategoryPreferences = jobCategoryPreferences;}
+    public void addJobCategoryPreference(String jobCategory){this.jobCategoryPreferences.add(jobCategory);}
+    public ArrayList<String> getJobCategoryPreferences(){return this.jobCategoryPreferences;}
+    public void removeJobCategoryPreference(String jobCategory){this.jobCategoryPreferences.remove(jobCategory);}
 }
