8dd2a7a5936739d96bdb643df2715994ffcdfb08
Renayre <>
2020-11-29 16:39:25 -0400

    [US10][AT10.1] Make a payment push notification start - removed alert dialog functionality and ui

--- app/src/main/java/com/example/group_7_proj/MakePaymentPopupActivity.java
+++ app/src/main/java/com/example/group_7_proj/MakePaymentPopupActivity.java
@@ -1,21 +1,49 @@
 package com.example.group_7_proj;
 
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+//login /  or on db add?
+//notification for new preferred jobs,
+// **notification for completed jobs / make a payment, DISMISS options
+// **notification for paid jobs or money deposited / DISMISS option
 
 public class MakePaymentPopupActivity extends AppCompatActivity {
     private String jobName;
+    private String contentTitle;
+    private String contentText;
+    private Intent intent;
+    private PendingIntent pendingIntent;
 
-    AlertDialog.Builder builder;
+    NotificationCompat.Builder builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.make_payment_popup);
+        //setContentView(R.layout.make_payment_popup);
+        jobName = "JobName"; //get job name from parameter pass?
+        contentTitle = "Job Completed"; //new String(jobName+ " has been completed")
+        contentText = new String(jobName + " has been completed.");
 
-        builder = new AlertDialog.Builder(this);
+        intent = new Intent(this, this.getClass()); //created job post page
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        pendingIntent = PendingIntent.getActivity(this, 0, intent, 0); //created job post page
+
+
+        builder = new NotificationCompat.Builder(this)
+        .setContentTitle(contentTitle)
+        .setContentText(contentText)
+        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        .setContentIntent(pendingIntent)
+        .addAction(R.drawable.ic_money, "Make A Payment", pendingIntent);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(1, builder.build());
 
     }
 }
