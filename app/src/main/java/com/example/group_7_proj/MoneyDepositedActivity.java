d2cd9b4567d422b0dbdc21278a8d5347849171db
Renayre <>
2020-11-29 16:39:53 -0400

    [US9][AT9.2] Money deposited push notification start - removed alert dialog functionality and ui

--- app/src/main/java/com/example/group_7_proj/MoneyDepositedActivity.java
+++ app/src/main/java/com/example/group_7_proj/MoneyDepositedActivity.java
@@ -1,9 +1,12 @@
 package com.example.group_7_proj;
 
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
 //job name from list of job posts paid
 //on paid trigger popup for respective employee
@@ -23,17 +26,31 @@ import androidx.appcompat.app.AppCompatActivity;
  * jobName = corresponding job name of job payed to employee
  */
 public class MoneyDepositedActivity extends AppCompatActivity {
-
     private String jobName;
+    private String contentTitle;
+    private String contentText;
+    private Intent intent;
+    private PendingIntent pendingIntent;
 
-    AlertDialog.Builder builder;
+    NotificationCompat.Builder builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.money_deposited_popup);
+        //setContentView(R.layout.make_payment_popup);
+        jobName = "JobName"; //get job name from parameter pass?
+        contentTitle = "Job Completed"; //new String(jobName+ " has been completed")
+        contentText = new String("Money desposited for " + jobName +"!");
 
-        builder = new AlertDialog.Builder(this);
+        intent = new Intent(this, this.getClass()); //accepted job post page
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        pendingIntent = PendingIntent.getActivity(this, 0, intent, 0); //accepted job post page
 
+
+        builder = new NotificationCompat.Builder(this)
+                .setContentTitle(contentTitle)
+                .setContentText(contentText)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent);
     }
 }
